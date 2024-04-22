c62cc936d8ff3bad2af635ca645f90173bba2b8a
Jose Latapiatt <jose.latapiatt@proton.me>
2024-04-22 02:07:41 -0400

    Desafio Evaluado 3

---
+++ desafio-03/script.sql
@@ -0,0 +1,67 @@
+-- Punto 1
+
+CREATE DATABASE peliculas;
+\c peliculas;
+
+CREATE DATABASE peliculas_testing;
+\c peliculas_testing;
+
+-- Punto 2
+
+CREATE TABLE peliculas (
+    id INT UNIQUE NOT NULL,
+    Pelicula VARCHAR,
+    "Año estreno" INT,
+    Director VARCHAR
+);
+
+COPY peliculas (id,Pelicula,"Año estreno",Director)
+FROM 'E:\proyectos\bootcamp\tests-desafiolatam-bdd\desafio-03\peliculas.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+CREATE TABLE reparto (
+    id_pelicula INT NOT NULL,
+    actor VARCHAR
+);
+
+COPY reparto (id_pelicula,actor)
+FROM 'E:\proyectos\bootcamp\tests-desafiolatam-bdd\desafio-03\reparto.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+-- punto 3
+SELECT id, Pelicula
+FROM peliculas 
+WHERE Pelicula = 'Titanic';
+
+-- Punto 4
+
+SELECT Pelicula, actor 
+FROM peliculas INNER JOIN reparto 
+ON peliculas.id = reparto.id_pelicula
+WHERE Pelicula = 'Titanic';
+
+-- Punto 5 se asume que los actores solo aparecen una vez por pelicula en el csv de reparto
+
+SELECT COUNT(actor), actor
+FROM reparto
+WHERE actor = 'Harrison Ford'
+GROUP BY actor;
+
+-- Punto 6
+
+SELECT * 
+FROM peliculas
+WHERE "Año estreno" >= 1990 AND "Año estreno" <= 1999
+ORDER BY Pelicula;
+
+-- Punto 7
+
+SELECT Pelicula, LENGTH(Pelicula) as longitud_titulo
+FROM peliculas;
+
+-- Punto 8 no preguntan por el nombre de la pelicula, solo la longitud
+
+SELECT MAX(LENGTH(Pelicula))
+FROM peliculas;
--- tests.sql
+++ tests.sql
@@ -138,3 +138,167 @@ UPDATE cuentas set balance = balance + 1000 where numero_cuenta = 1;
 select * from cuentas;
 
 rollback;
+
+--------------------------------------------------------------------------------
+create database d8;
+\c d8;
+
+
+create table clientes (id integer unique not null, name varchar(25) not null, email varchar(50));
+
+insert into clientes(id, name) values (1, 'Nombre 1');
+insert into clientes(id, name) values (2, 'Nombre 2');
+insert into clientes(id, name) values (3, 'Nombre 3');
+
+ALTER TABLE clientes ALTER COLUMN email SET NOT NULL;
+
+UPDATE clientes SET email = '' WHERE email IS NULL;
+
+ALTER TABLE clientes ADD COLUMN fecha date;
+
+UPDATE clientes SET fecha = COALESCE(fecha, '2024-01-01');
+
+ALTER TABLE clientes ALTER COLUMN fecha SET NOT NULL;
+
+--------------------------------------------------------------------------------
+
+CREATE TABLE productos (
+  id serial PRIMARY KEY,
+  nombre varchar(255) NOT NULL,
+  precio numeric(10,2) NOT NULL,
+  sku text
+);
+
+INSERT INTO productos (nombre, precio, sku)
+VALUES
+  ('Televisor 50 pulgadas', 1500.00, NULL),
+  ('Celular de alta gama', 1000.00, NULL),
+  ('Laptop de última generación', 2000.00, NULL),
+  ('Nevera con congelador', 500.00, NULL),
+  ('Lavadora automática', 300.00, NULL),
+  ('Cocina a gas', 400.00, NULL),
+  ('Horno microondas', 250.00, NULL),
+  ('Juego de ollas', 100.00, NULL),
+  ('Set de cubiertos', 50.00, NULL),
+  ('Cafetera eléctrica', 75.00, NULL),
+  ('Tostadora', 50.00, NULL),
+  ('Plancha de vapor', 100.00, NULL),
+  ('Secadora de ropa', 200.00, NULL),
+  ('Aspiradora', 150.00, NULL),
+  ('Robot aspirador', 300.00, NULL),
+  ('Silla de oficina', 200.00, NULL),
+  ('Mesa de escritorio', 150.00, NULL),
+  ('Escritorio para computadora', 250.00, NULL),
+  ('Sillón para sala', 300.00, NULL),
+  ('Sofá cama', 400.00, NULL),
+  ('Mesa de centro', 200.00, NULL),
+  ('Silla para comedor', 150.00, NULL),
+  ('Mesa para comedor', 250.00, NULL),
+  ('Cama matrimonial', 400.00, NULL),
+  ('Cabecera para cama', 250.00, NULL),
+  ('Mesa de noche', 150.00, NULL),
+  ('Armario ropero', 300.00, NULL),
+  ('Espejo para baño', 200.00, NULL),
+  ('Inodoro', 150.00, NULL),
+  ('Lavabo', 250.00, NULL),
+  ('Grifería para baño', 100.00, NULL),
+  ('Ducha', 75.00, NULL),
+  ('Bañera', 50.00, NULL),
+  ('Cocina de inducción', 400.00, NULL),
+  ('Campana extractora', 300.00, NULL),
+  ('Microondas', 250.00, NULL),
+  ('Lavavajillas', 1000.00, NULL),
+  ('Lavadora secadora', 2000.00, NULL);
+
+UPDATE productos SET sku = COALESCE(sku, 'Dato no ingresado');
+
+
+ALTER TABLE productos ALTER COLUMN sku SET NOT NULL;
+
+--------------------------------------------------------------------------------
+
+CREATE TABLE autores (
+id INT NOT NULL,
+nombre VARCHAR(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE libros (
+id INT NOT NULL,
+titulo VARCHAR(255) NOT NULL,
+autor_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (autor_id) REFERENCES autores (id)
+);
+
+INSERT INTO autores (id, nombre)
+VALUES
+(1, 'Juan Pérez'),
+(2, 'María García'),
+(3, 'Pedro Rodríguez');
+
+INSERT INTO libros (id, titulo, autor_id)
+VALUES
+(1, 'El Quijote', 1),
+(2, 'La Divina Comedia', 2),
+(3, 'Hamlet', 3);
+
+SELECT libros.titulo, autores.nombre
+FROM libros
+INNER JOIN autores
+ON libros.autor_id = autores.id;
+
+delete from autores;
+delete from libros;
+
+----------------------- d9
+
+
+create database mawashi_phones;
+\c mawashi_phones;
+
+create table phones (
+	id int primary key, 
+	modelo varchar(50), 
+	mac_address int unique,
+	fecha_fabricacion varchar(50));
+
+insert into phones (id, modelo, mac_address, fecha_fabricacion) 
+values (
+	1, 
+	'Iphone 14',
+	'1B:2A:3C:4D:5F:6G',
+	'2022-09-09');
+
+alter table phones alter column mac_address type varchar(50);
+
+\d phones;
+
+alter table phones alter column mac_address set  NOT NULL;
+
+insert into phones (id, modelo, mac_address, fecha_fabricacion) values (2, 'Iphone
+13', '5B:1A:2C:7D:8F:7h', '2021-09-24');
+
+truncate phones;
+
+select * from phones;
+
+--------------------------------------------------------------------------------
+
+ALTER TABLE phones
+ADD COLUMN memoria_interna VARCHAR NOT NULL,
+ADD COLUMN memoria_ram VARCHAR NOT NULL,
+ADD COLUMN peso VARCHAR NOT NULL,
+ADD COLUMN dimensiones VARCHAR NOT NULL;
+
+
+-- ALTER COLUMN id TYPE serial;
+
+-- Create the sequence.
+CREATE SEQUENCE public."id_seq" OWNED BY public.phones.id;
+
+-- Assign it to the table default value.
+ALTER TABLE phones ALTER COLUMN id SET DEFAULT nextval('public."id_seq"');
+
+select COUNT(id),  from peliculas where "Director" = 'Peter Jackson';
+select COUNT(id), "Pelicula", "Año estreno"  from peliculas where "Director" = 'Peter Jackson';
